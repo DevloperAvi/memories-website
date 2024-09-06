229e47c423f56cf9b3abdcb98c0221ed13b7db34
ARVIND SAHU <132640358+DevloperAvi@users.noreply.github.com>
2024-09-07 00:58:40 +0530

    all files
    na

---
+++ scripts.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+    const memories = `
+        Here you can add the special memories you have shared. For example:
+        - The first time we met at the café.
+        - That beautiful walk in the park.
+        - Our movie night where we couldn't stop laughing.
+        - The surprise gift you gave me on my birthday.
+
+        And so much more to come...
+    `;
+    document.getElementById('storyContent').innerText = memories;
+};
